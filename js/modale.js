815ad7213319ac6b45321bf9e2f3ed319225aabe
Cosmin Dumitriu <47316223+cossminuu@users.noreply.github.com>
2019-10-08 12:23:37 +0200

    Create modale.js

---
+++ js/modale.js
@@ -0,0 +1,6 @@
+function modale() {
+            //usiamo le animazioni di jQuery
+            $(".overlay, .finestra").fadeToggle(400);
+        }
+        $(".apri, .overlay, .close").click(modale);
+
